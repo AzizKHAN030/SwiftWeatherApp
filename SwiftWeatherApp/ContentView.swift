fb3142acd41f65ecc5abf0fb39ba35123ac3441b
Azizbek Rasulov <azizbek.rasulov@scandiweb.com>
2024-01-07 20:04:26 +0500

    Initial Commit

---
+++ SwiftWeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftWeatherApp
+//
+//  Created by Azizbek  Rasulov on 07/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftWeatherApp/SwiftWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWeatherAppApp.swift
+//  SwiftWeatherApp
+//
+//  Created by Azizbek  Rasulov on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
